43e24cc964853fadf698190bee9ca8ec64130530
randika-chathuranga <randikachathuranga219@gmail.com>
2021-02-27 22:53:49 +0530

    added all the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import Logo from './unnamed.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       newItem:"",
+       list:[]
+    }
+  }
+
+  addItem(todoValue){
+    if (todoValue !== "") {
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false,
+      };
+
+      const list = [...this.state.list];
+      list.push(newItem)
+
+      this.setState({
+        list: list,
+        newItem:""
+      })
+    }
+  }
+
+  deleteItem(id){
+    const list=[...this.state.list];
+    const updatedlist = list.filter(item=> item.id !== id);
+    this.setState({
+      list:updatedlist
+    })
+  }
+
+  updateInput(input){
+    this.setState({
+      newItem:input
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <div className="App">
+      <img src={Logo} alt="todologo" width="200" height="100" className="logo"/>
+      <h1 className="app-title">Todo app</h1>
+
+      <div className="container">
+        add an item
+        <br/>
+        <input type="text" className="input-text" placeholder="write a todo" required value={this.state.newItem} onChange={e=>this.updateInput(e.target.value)}/>
+        <button onClick={()=>this.addItem(this.state.newItem)} disabled={!this.state.newItem.length} className="add-btn">Add Todo</button>
+      </div>
+
+      <div className="list">
+        <ul>
+          {this.state.list.map(item=>{
+            return(
+              <li key={item.id}>
+                <input type="checkbox" name="idDone" checked={item.isDone} onChange={()=>{}}/>
+                {item.value}
+                <button className="btn" onClick={()=>this.deleteItem(item.id)}>Delete</button>
+              </li>
+            )
+          })}
+          <li>
+            <input type="checkbox"/>Record youtube videos
+            <button className="btn">delete</button>
+          </li>
+        </ul>
+      </div>
+      
+
     </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
